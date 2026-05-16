29b7e542b53691239705c74f06d11281a7652d29
Umangsharma15 <umangpandittt15@gmail.com>
2026-05-16 11:10:15 +0530

    fixed streamlit deployment

--- agents.py
+++ agents.py
@@ -4,13 +4,15 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
 #model setup 
+
 llm = ChatMistralAI(
     model="mistral-small-latest",
-    temperature=0.7
+    api_key=st.secrets["MISTRAL_API_KEY"]
 )
 
 
